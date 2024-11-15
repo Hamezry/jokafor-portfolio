4422da792eb53bdba1711b49a202de92c774d8c5
James Okafor <jokafor@afexnigeria.com>
2024-11-15 11:36:19 +0100

    added resume

--- components/home/experience/data.ts
+++ components/home/experience/data.ts
@@ -1,5 +1,29 @@
 export const experiences = [
   {
+    duration: "September 2024 - Present",
+    company: "Sahel AI",
+    title: "Frontend Developer",
+    website: "https://www.linkedin.com/company/sahel-ai/",
+    location: "Dover, DE 19901, USA",
+    descriptions: [
+      "Development, and implementation of the UI/UX, layout.",
+      "Aided in the assessment of all development technologies in order to ensure quality and accuracy of the project.",
+      "Optimized frontend performance to improve load times and enhance the overall user experience.",
+      "Implemented lazy loading, code splitting, and efficient state management to ensure smooth performance even with high data volumes on the systems.",
+      "Ensured close coordination with backend developers to outline API needs and guarantee smooth interaction between frontend and backend systems.",
+      "Participated in Agile development processes, including sprint planning, daily stand-ups, and retrospectives to deliver high-quality software on time.",
+    ],
+    techs: [
+      "JavaScript",
+      "TypeScript",
+      "TailwindCSS",
+      "React.js",
+      "Next.js",
+      "Cypress",
+      "GraphQl",
+    ],
+  },
+  {
     duration: "July 2022 - Present",
     company: "AFEX",
     title: "Frontend Developer",
@@ -10,8 +34,8 @@ export const experiences = [
       "Aided in the assessment of all development technologies in order to ensure quality and accuracy of the project.",
       "Optimized frontend performance to improve load times and enhance the overall user experience.",
       "Implemented lazy loading, code splitting, and efficient state management to ensure smooth performance even with high data volumes on the systems.",
-	  "Ensured close coordination with backend developers to outline API needs and guarantee smooth interaction between frontend and backend systems.",
-	  "Participated in Agile development processes, including sprint planning, daily stand-ups, and retrospectives to deliver high-quality software on time."
+      "Ensured close coordination with backend developers to outline API needs and guarantee smooth interaction between frontend and backend systems.",
+      "Participated in Agile development processes, including sprint planning, daily stand-ups, and retrospectives to deliver high-quality software on time.",
     ],
     techs: [
       "JavaScript",
@@ -57,6 +81,9 @@ export const experiences = [
     techs: ["JavaScript", "TypeScript", "React.js"],
   },
 ];
+
+
+
 
 
 
